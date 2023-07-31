0a2f469b72120e9c80de0ee61904860b99beedaf
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-07-31 11:30:46 +0300

    1. Ветки [#504827 # [#504827]]

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -2,12 +2,10 @@ package ru.job4j;
 
 /**
  * Hello world!
- *
+ *добавить изменения в последний коммит
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
