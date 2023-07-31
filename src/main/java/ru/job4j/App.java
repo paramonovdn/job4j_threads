5a808c36cab67ef954171c418536a955ae6703c9
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-07-31 12:46:19 +0300

    1. Ветки [#504827 # [#504827]]

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -4,6 +4,8 @@ package ru.job4j;
  * Hello world!
  *добавить изменения в последний коммит
  * 1 изменение
+ * 2 изменение
+ * 3 изменение
  */
 public class App {
     public static void main(String[] args) {
