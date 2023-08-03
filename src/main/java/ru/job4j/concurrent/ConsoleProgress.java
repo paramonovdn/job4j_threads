ba372131436f3e36e0d5b7c5fb9a7591ed58c164
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-08-03 11:54:22 +0300

    5. Прерывание нити [#1019 # [#1019]]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                for (int i = 0; i <= 3; i++) {
+                    System.out.print("\r load: " + process[i]);
+                    Thread.sleep(500);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000); /** симулируем выполнение параллельной задачи в течение 5 секунд. */
+        progress.interrupt();
+    }
+}
