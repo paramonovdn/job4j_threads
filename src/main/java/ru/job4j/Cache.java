d4a71fcd00aed670f3cbad6ff84717cec72a9a92
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-08-08 18:44:44 +0300

    11. Отладка в многопоточном приложении [#504954 #435556]

--- src/main/java/ru/job4j/Cache.java
+++ src/main/java/ru/job4j/Cache.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public final class Cache {
     private static Cache cache;
 
-    public static Cache instOf() {
+    public synchronized static Cache instOf() {
         if (cache == null) {
             cache = new Cache();
         }
