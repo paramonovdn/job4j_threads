b3ac3841a640f82ae6e8aa989d66e87dc14ccb3b
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-08-08 18:43:42 +0300

    11. Отладка в многопоточном приложении [#504954 #435556]

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,29 +1,7 @@
 package ru.job4j;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.IntStream;
-
-/**
- * Текущее состояние
- * Изменения 2
- */
 public class App {
     public static void main(String[] args) {
-        //List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        //list.stream().parallel().peek(System.out::println).toList();
-        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
-        String[] parts = url.split("/");
-        System.out.println(parts[0]);
-        System.out.println(parts[1]);
-        System.out.println(parts[2]);
-        System.out.println(parts[3]);
-        System.out.println(parts[4]);
-        System.out.println(parts[5]);
-        System.out.println(parts[parts.length - 1]);
-
-
-
+        System.out.println("Hello world!");
     }
 }
