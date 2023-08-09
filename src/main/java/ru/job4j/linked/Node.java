faec44a39b4ceedcd8f349b872755778e1662a86
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-08-09 16:35:51 +0300

    3. Immutable объекты [#267918]

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,26 @@
+package ru.job4j.linked;
+
+public final class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public Node(T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+    }
+
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+}
