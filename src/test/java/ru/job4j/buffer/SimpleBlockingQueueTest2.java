cb871b7f1cbed425d942d6a41f987096759bc95b
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-08-28 12:50:21 +0300

    3. Junit тест для блокирующей очереди. [#68589]

---
+++ src/test/java/ru/job4j/buffer/SimpleBlockingQueueTest2.java
@@ -0,0 +1,42 @@
+package ru.job4j.buffer;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class SimpleBlockingQueueTest2 {
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 10; i >0; i--) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).containsExactly(10,9,8,7,6,5,4,3,2,1);
+    }
+}
